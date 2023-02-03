729dcb88ff4001d5a15a94aeb5dee4ec81f10101
Alaa Saad Mansour <alaasaadabdo@gmail.com>
2023-02-04 00:02:08 +0100

    refactor: batreeq OG to have 3 layer only

--- keyboards/mabroum/batreeq/keymaps/alaa/keymap.c
+++ keyboards/mabroum/batreeq/keymaps/alaa/keymap.c
@@ -31,17 +31,15 @@ enum layers {
 // layers
 #define MO_NAV MO(1)
 #define MO_SYM MO(2)
-#define MO_NAVW MO(4)
-#define MO_SYMW MO(5)
 
 // mac
-#define PST         G(KC_V)
-#define CPY         G(KC_C)
-#define CUT         G(KC_X)
-#define SALL        G(KC_A)
-#define DWRD        A(KC_BSPC)
-#define QUIT        G(KC_Q)
-#define SAVE        G(KC_S)
+#define PSTM         G(KC_V)
+#define CPYM         G(KC_C)
+#define CUTM         G(KC_X)
+#define SALLM        G(KC_A)
+#define DWRDM        A(KC_BSPC)
+#define QUITM        G(KC_Q)
+#define SAVEM        G(KC_S)
 
 //windows
 #define PSTW         C(KC_V)
@@ -76,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NAV] = LAYOUT_23332(
         SCRNSHT,   KC_ENT, KC_BSPC, KC_TAB,  KC_DEL,            DWRD,    KC_NO,     KC_UP,    QUIT,       WRKFLW1,
         OS_LCTL,   OS_LALT, OS_LGUI,  OS_LSFT, SALL,            KC_DOT,  KC_LEFT,   KC_DOWN,  KC_RIGHT,   WRKFLW2,
-                   CUT,    CPY,     PST,                                 KC_ENT,    KC_BSPC,  KC_TAB,
+                   CUT,    COPY,     PASTE,                                 KC_ENT,    KC_BSPC,  KC_TAB,
                                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
@@ -87,36 +85,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-    [HDNW] = LAYOUT_23332(
-        KC_W,   KC_F,   KC_M,   KC_P,   KC_B,                   KC_COMMA, KC_X,   KC_QUOTE, KC_J,   KC_K,
-        KC_R,   KC_S,   KC_N,   KC_T,   KC_G,                   KC_DOT,   KC_A,   KC_E,     KC_I,   KC_H,
-                KC_C,   KC_L,   KC_D,                                     KC_U,   KC_O,     KC_Y,
-                                        MO_NAVW, KC_SPACE,  OSM(MOD_LSFT), MO_SYMW,
-                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
-    [NAVW] = LAYOUT_23332(
-        SCRNSHT,   KC_ENT, KC_BSPC, KC_TAB,  KC_DEL,            DWRD,    KC_F2,     KC_UP,    QUITW,      DPI_MOD,
-        OS_LGUI,   OS_LALT, OS_LCTL, OS_LSFT, SALLW,            SALLW,   KC_LEFT,   KC_DOWN,  KC_RIGHT,   RCRD,
-                   CUTW,   CPYW,    PSTW,                                KC_ENT,    KC_BSPC,  KC_TAB,
-                                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [SYMW] = LAYOUT_23332(
-        KC_TILDE,   KC_9,   KC_8,   KC_7,   KC_EQUAL,           KC_COMMA, KC_SCLN, KC_COLN, KC_BSLS, KC_GRAVE,
-        KC_3,       KC_2,   KC_1,   KC_0,   KC_MINUS,           KC_DOT,   OS_LSFT,     OS_LCTL,     OS_RALT,     OS_LGUI,
-                    KC_6,   KC_5,   KC_4,                                 KC_QUES, KC_EXLM, KC_SLSH,
-                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    )
 };
 
 bool is_nshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
     case MO_SYM:
     case MO_NAV:
-    case MO_SYMW:
-    case MO_NAVW:
     case OS_LCTL:
     case OS_LALT:
     case OS_RALT:
@@ -132,8 +106,6 @@ bool is_nshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
     case MO_SYM:
     case MO_NAV:
-    case MO_SYMW:
-    case MO_NAVW:
         return true;
     default:
         return false;
@@ -154,9 +126,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_DEL:
         case KC_UNDS:
         case MO_SYM:
-        case MO_SYMW:
         case MO_NAV:
-        case MO_NAVW:
         case OS_LSFT:
         case OS_LCTL:
         case OS_LALT:
@@ -176,64 +146,117 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     process_nshot_state(keycode, record);
     mab_process_record_haptic(keycode, record);
     mab_process_pointing_keys(keycode, record);
-    switch(get_highest_layer(default_layer_state)) {
-        case HDN:
-            update_swapper(
-                &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
-                keycode, record
-            );
-            break;
-        case HDNW:
-            update_swapper(
-                &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
-                keycode, record
-            );
-            break;
-    }
-    switch (keycode) {
-        case UNDO:
-            if (record->event.pressed) {
-                switch(get_highest_layer(default_layer_state)) {
-                    case HDN:
-                        tap_code16(UNDOMAC);
-                        break;
-                    case HDNW:
-                        tap_code16(UNDOWIN);
-                        break;
-                }
-            }
-            break;
-        case REDO:
-            if (record->event.pressed) {
-                switch(get_highest_layer(default_layer_state)) {
-                    case HDN:
-                        tap_code16(REDOMAC);
-                        break;
-                    case HDNW:
-                        tap_code16(REDOWIN);
-                        break;
-                }
-            }
-            break;
-        case WIN_LAYER:
-            if (record->event.pressed) {
-              set_single_persistent_default_layer(3);
-              mab_set_pointer_win();
-            }
-            break;
-        case MAC_LAYER:
-            if (record->event.pressed) {
-              set_single_persistent_default_layer(0);
-              mab_set_pointer_mac();
-            }
-            break;
-    }
     process_repeat_key(keycode, record);
     // It's important to update the mod variables *after* calling process_repeat_key, or else
     // only a single modifier from the previous key is repeated (e.g. Ctrl+Shift+T then Repeat produces Shift+T)
     mod_state = get_mods();
     oneshot_mod_state = get_oneshot_mods();
 
+    if (keymap_config.swap_lctl_lgui) {
+            update_swapper(
+                &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
+                keycode, record
+            );
+    } else {
+            update_swapper(
+                &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
+                keycode, record
+            );
+    }
+    switch (keycode) {
+        case DWRD:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(DWRDM);
+                } else {
+                        tap_code16(DWRDW);
+                }
+            }
+            return false;
+        case QUIT:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(QUITM);
+                } else {
+                        tap_code16(QUITW);
+                }
+            }
+            return false;
+        case SAVE:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(SAVEM);
+                } else {
+                        tap_code16(SAVEW);
+                }
+            }
+            return false;
+        case SALL:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(SALLM);
+                } else {
+                        tap_code16(SALLW);
+                }
+            }
+            return false;
+        case COPY:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(CPYM);
+                } else {
+                        tap_code16(CPYW);
+                }
+            }
+            return false;
+        case CUT:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(CUTM);
+                } else {
+                        tap_code16(CUTW);
+                }
+            }
+            return false;
+        case PASTE:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(PSTM);
+                } else {
+                        tap_code16(PSTW);
+                }
+            }
+            return false;
+        case UNDO:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(UNDOMAC);
+                } else {
+                        tap_code16(UNDOWIN);
+                }
+            }
+            return false;
+        case REDO:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(REDOMAC);
+                } else {
+                        tap_code16(REDOWIN);
+                }
+            }
+            return false;
+        case SW_CG:
+            if (record->event.pressed) {
+              keymap_config.raw = eeconfig_read_keymap();
+              keymap_config.swap_lctl_lgui = !keymap_config.swap_lctl_lgui;
+              keymap_config.swap_rctl_rgui = keymap_config.swap_lctl_lgui;
+              eeconfig_update_keymap(keymap_config.raw);
+              mab_set_pointer();
+            }
+            return false;
+        default:
+            return true;
+    }
     return true;
 }
 
@@ -241,7 +264,4 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [HDN]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
     [NAV]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
     [SYM]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
-    [HDNW] =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
-    [NAVW] =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
-    [SYMW] =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)}
 };
