8a0a568e39c1cd8bad0c4b876ad5c4a1144476d2
Alabastard-64 <snipac+github@gmail.com>
2023-03-05 23:11:28 -0700

    big update to multi device control

--- quantum/pointing_device/pointing_device.c
+++ quantum/pointing_device/pointing_device.c
@@ -468,24 +468,24 @@ report_mouse_t pointing_device_adjust_by_defines_right(report_mouse_t mouse_repo
  */
 report_mouse_t pointing_device_task_combined(report_mouse_t left_report, report_mouse_t right_report) {
 #    ifdef POINTING_DEVICE_MODES_ENABLE
-#        ifdef POINTING_MODES_SINGLE_CONTROL
+#        if POINTING_MODES_SINGLE_CONTROL
     // only one side controlled at any one time
     switch (get_pointing_mode_device()) {
-        case PM_RIGHT_SIDE:
+        case PM_RIGHT_DEVICE:
             right_report = pointing_device_modes_task(right_report);
             break;
         default:
             left_report = pointing_device_modes_task(left_report);
-    }
+    }[
 #        else
     // both sides controlled independently
     // save current device id
     uint8_t current_device = get_pointing_mode_device();
-    set_pointing_mode_device(PM_RIGHT_SIDE);
+    set_pointing_mode_device(PM_RIGHT_DEVICE);
     right_report = pointing_device_modes_task(right_report);
 
-    set_pointing_mode_device(PM_LEFT_SIDE);
-    left_report = pointing_device_modes_task(left_report);
+    set_pointing_mode_device(PM_LEFT_DEVICE);
+    left_report = pointing_device_modes_task(left_report);]
     // set device id back
     set_pointing_mode_device(current_device);
 #        endif
