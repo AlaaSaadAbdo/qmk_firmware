842a2da47445d3cd7401c996f4dce9d118508a77
Alaa Saad Mansour <AlaaSaadAbdo@users.noreply.github.com>
2023-03-27 19:52:30 +0200

    qwetry on Zazu's default layout

--- keyboards/mabroum/zazu/keymaps/default/keymap.c
+++ keyboards/mabroum/zazu/keymaps/default/keymap.c
@@ -64,9 +64,9 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [HDN] = LAYOUT_2_3x5_3(
-        KC_W, KC_W,  KC_F,   KC_M,   KC_P,   KC_B,                   KC_SLSH,   KC_X,   KC_QUOTE, KC_J, KC_K,   KC_K,
-        KC_R, KC_R,  KC_S,   KC_N,   KC_T,   KC_G,                   KC_COMMA,  KC_A,   KC_E,     KC_I, KC_H,   KC_H,
-              KC_Z,  KC_C,   KC_L,   KC_D,   KC_V,                   KC_DOT,    KC_U,   KC_O,     KC_Y, KC_J,
+        KC_W, KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,      KC_U,   KC_I,     KC_O,    KC_P,       KC_K,
+        KC_A, KC_S,  KC_D,   KC_F,   KC_G,   KC_H,                   KC_J,      KC_K,   KC_L,     KC_SCLN, KC_QUOTE,   KC_H,
+              KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,      KC_M,   KC_COMMA, KC_DOT,  KC_SLSH,
                                 MO_NAV, KC_SPACE,  KC_DOT, KC_COMMA,  OSM(MOD_LSFT), MO_SYM,
                                         KC_BTN3, KC_BTN1, KC_BTN2,
                                      // click  ,  right           , down    , left          , up
