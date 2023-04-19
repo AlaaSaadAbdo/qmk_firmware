ab04bed29d87ecb819e8bac90b6b962e31b50f0c
Alaa Saad Mansour <AlaaSaadAbdo@users.noreply.github.com>
2023-04-19 09:41:21 +0200

    remove swapper timer

--- keyboards/mabroum/wizza/keymaps/alaa/swapper.c
+++ keyboards/mabroum/wizza/keymaps/alaa/swapper.c
@@ -1,7 +1,5 @@
 #include "swapper.h"
 
-uint32_t swapper_timer = 0;
-
 void update_swapper(
     bool *active,
     uint16_t cmdish,
@@ -15,15 +13,10 @@ void update_swapper(
             if (!*active) {
                 *active = true;
                 register_code(cmdish);
-                swapper_timer = timer_read32();
             }
             register_code(tabish);
         } else {
             unregister_code(tabish);
-            // unregister alt after a timer
-            if (timer_elapsed32(swapper_timer) > 1000 ) {
-                unregister_code(cmdish);
-            }
         }
     } else if (*active) {
         unregister_code(cmdish);
