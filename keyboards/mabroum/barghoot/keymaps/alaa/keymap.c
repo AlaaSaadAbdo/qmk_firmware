25fe8e591733dfd77392bbaabcf76203085cd451
Alaa Saad Mansour <alaasaadabdo@gmail.com>
2023-05-15 07:47:43 +0200

    add barghoot to auto build

--- keyboards/mabroum/barghoot/keymaps/alaa/keymap.c
+++ keyboards/mabroum/barghoot/keymaps/alaa/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_W,  KC_F,   KC_M,   KC_P,   KC_B,                   KC_SLSH,   KC_X,   KC_QUOTE, KC_J, KC_K,
         KC_R,  KC_S,   KC_N,   KC_T,   KC_G,                   KC_COMMA,  KC_A,   KC_E,     KC_I, KC_H,
         KC_Z,  KC_C,   KC_L,   KC_D,   KC_V,                   KC_DOT,    KC_U,   KC_O,     KC_Y, KC_J,
-    KC_BTN3, KC_BTN1, KC_BTN2, MO_NAV, KC_SPACE,               OSM(MOD_LSFT), MO_SYM, KC_BTN3, KC_BTN1, KC_BTN2,
+    KC_BTN3, KC_BTN2, KC_BTN1, MO_NAV, KC_SPACE,               OSM(MOD_LSFT), MO_SYM, KC_BTN1, KC_BTN2, KC_BTN3,
                           KC_0,        KC_BTN3, KC_BTN1, KC_BTN2, KC_0
     ),
 
