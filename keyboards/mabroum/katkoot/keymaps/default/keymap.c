3037dc68ba4ecc0591e710f8e262c8cdb3a4d87b
Alaa Saad Mansour <alaasaadabdo@gmail.com>
2024-06-17 23:49:32 +0200

    fix dragscroll

--- keyboards/mabroum/katkoot/keymaps/default/keymap.c
+++ keyboards/mabroum/katkoot/keymaps/default/keymap.c
@@ -1,8 +1,8 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "keyboards/mabroum/mab_pointing.h"
 #include "keyboards/mabroum/keys.h"
+#include "keyboards/mabroum/mab_pointing.h"
 
 
 #define SNIPING SNIPING_MODE
@@ -23,21 +23,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_2x3(
-        DRG_TOG,   KC_BTN3,   KC_BTN2,
-        SNP_TOG,   KC_BTN1,   KC_KB_MUTE
+        KC_BTN3,   KC_BTN1,   DRG_TOG,
+        SNP_TOG,   KC_BTN2,   KC_MUTE
     )
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    mab_process_pointing_keys(keycode, record);
+    return true;
+}
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0]  =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  debug_mouse=true;
-}
+/* void keyboard_post_init_user(void) { */
+/*   // Customise these values to desired behaviour */
+/*   debug_enable=true; */
+/*   debug_matrix=true; */
+/*   debug_keyboard=true; */
+/*   debug_mouse=true; */
+/* } */
 
