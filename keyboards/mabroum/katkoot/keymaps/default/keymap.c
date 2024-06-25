a87b9baf0e0936f4c4bd1218b38f0d89a86911a1
Alaa Saad Mansour <AlaaSaadAbdo@users.noreply.github.com>
2024-06-25 16:57:46 +0200

    keymap for hand wired button

--- keyboards/mabroum/katkoot/keymaps/default/keymap.c
+++ keyboards/mabroum/katkoot/keymaps/default/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_2x3(
-        KC_BTN2,   KC_BTN3,   KC_BTN1,
-        DRG_TOG,   DRGSCRL,   KC_MUTE
+        DRGSCRL,   KC_BTN1,   KC_BTN3,
+        DRG_TOG,   KC_BTN2,   KC_MUTE
     )
 };
 
